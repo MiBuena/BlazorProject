5d0b068dbf25e95e1a3355e24a7df1b29edb29c0
MiBuena <athina.palada@gmail.com>
2020-08-23 20:20:44 +0300

    Add Item add razor component

---
+++ ListGenerator.Server/ListGenerator.Server/Pages/ItemAdd.cs
@@ -0,0 +1,26 @@
+﻿using ListGenerator.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListGenerator.Server.Pages
+{
+    public partial class ItemAdd
+    {
+        public Item ItemToAdd { get; set; } = new Item();
+
+
+
+        protected async Task HandleValidSubmit()
+        {
+            
+        }
+
+        protected void HandleInvalidSubmit()
+        {
+
+        }
+
+    }
+}
