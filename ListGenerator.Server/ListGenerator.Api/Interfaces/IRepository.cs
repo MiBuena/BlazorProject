9c2bf57f0e74459864ea74fa1985e2b98521be2b
MiBuena <athina.palada@gmail.com>
2020-08-25 17:19:26 +0300

    Add EfRepository

---
+++ ListGenerator.Server/ListGenerator.Api/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListGenerator.Api.Interfaces
+{
+    public interface IRepository<TEntity>
+      where TEntity : class
+    {
+        IQueryable<TEntity> All();
+
+        IQueryable<TEntity> AllAsNoTracking();
+
+        Task AddAsync(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        Task<int> SaveChangesAsync();
+    }
+}
