8d0dafb107755f5833efd414600783df305cd435
MiBuena <athina.palada@gmail.com>
2020-09-11 17:50:07 +0300

    Fixing date

---
+++ ListGenerator.Server/ListGenerator/Shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ListGenerator.Shared.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToDateString(this DateTime date)
+        {
+            return date.ToString("dd.MM.yyyy");
+        }
+    }
+}
