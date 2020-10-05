276dcc5b6191a40d4b01f72b6739b53e531fed24
MiBuena <athina.palada@gmail.com>
2020-10-05 20:22:33 +0300

    Add parent class BUnitTestsContext

--- ListGenerator.Server/ListGenerator.Web.UnitTests/SpinnerTests/ShowSpinnerTests.cs
+++ ListGenerator.Server/ListGenerator.Web.UnitTests/SpinnerTests/ShowSpinnerTests.cs
@@ -11,18 +11,17 @@ using System.Text;
 namespace ListGenerator.Web.UnitTests.SpinnerTests
 {
     [TestFixture]
-    public class ShowSpinnerTests
+    public class ShowSpinnerTests : BUnitTestContext
     {
         [Test]
         public void Should_HaveEmptyMarkUp_When_SpinnerIsNotShown()
         {
             //Arrange
-            using var ctx = new Bunit.TestContext();
             var mockSpinnerService = new Mock<SpinnerService>();
-            ctx.Services.AddSingleton(mockSpinnerService.Object);
+            Services.AddSingleton(mockSpinnerService.Object);
 
             //Act
-            var cut = ctx.RenderComponent<Spinner>();
+            var cut = RenderComponent<Spinner>();
 
             // Assert
             cut.MarkupMatches(string.Empty);
@@ -32,12 +31,11 @@ namespace ListGenerator.Web.UnitTests.SpinnerTests
         public void Should_HaveDivWithSpinnerContainerClassWithElements_When_SpinnerIsShown()
         {
             //Arrange
-            using var ctx = new Bunit.TestContext();
             var mockSpinnerService = new Mock<SpinnerService>();
-            ctx.Services.AddSingleton(mockSpinnerService.Object);
+            Services.AddSingleton(mockSpinnerService.Object);
 
             //Act
-            var cut = ctx.RenderComponent<Spinner>();
+            var cut = RenderComponent<Spinner>();
             cut.InvokeAsync(() => cut.Instance.ShowSpinner());
 
 
