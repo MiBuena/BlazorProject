87dad14c84c47641ce3cbcb7e6d29dcbb3a7b17d
MiBuena <athina.palada@gmail.com>
2020-10-14 18:02:36 +0300

    Add unit tests for when update parameters are valid

--- ListGenerator.Server/ListGenerator.Web.UnitTests/ItemsDataServiceTests/UpdateItemTests.cs
+++ ListGenerator.Server/ListGenerator.Web.UnitTests/ItemsDataServiceTests/UpdateItemTests.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using ListGenerator.Data.Entities;
+using ListGenerator.Shared.Dtos;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +16,74 @@ namespace ListGenerator.Web.UnitTests.ItemsDataServiceTests
         protected override void Init()
         {
             base.Init();
+        }
+
+        [Test]
+        public void Should_UpdateItem_When_InputParametersAreValid()
+        {
+            //Arrange
+            var allItems = BuildItemsCollection();
+            ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
+
+            var saveObject = new Item();
+            ItemsRepositoryMock.Setup(c => c.Update(It.IsAny<Item>()))
+                    .Callback<Item>((obj) => saveObject = obj);
+
+            ItemsRepositoryMock.Setup(c => c.SaveChanges());
+
+            var updatedItemDto = new ItemDto()
+            {
+                Id = 1,
+                Name = "Bread updated",
+                NextReplenishmentDate = new DateTime(2020, 10, 10),
+                ReplenishmentPeriod = 4
+            };
+
+
+            //Act
+            var result = ItemsDataService.UpdateItem("ab70793b-cec8-4eba-99f3-cbad0b1649d0", updatedItemDto);
+
+
+            //Assert
+            AssertHelper.AssertAll(
+                () => saveObject.Id.Should().Be(1),
+                () => saveObject.Name.Should().Be("Bread updated"),
+                () => saveObject.NextReplenishmentDate.Should().BeSameDateAs(new DateTime(2020, 10, 10)),
+                () => saveObject.ReplenishmentPeriod.Should().Be(4)
+                );
+        }
+
+        [Test]
+        public void Should_ReturnSuccessResponse_When_InputParametersAreValid()
+        {
+            //Arrange
+            var allItems = BuildItemsCollection();
+            ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
+
+            var saveObject = new Item();
+            ItemsRepositoryMock.Setup(c => c.Update(It.IsAny<Item>()))
+                    .Callback<Item>((obj) => saveObject = obj);
+
+            ItemsRepositoryMock.Setup(c => c.SaveChanges());
+
+            var updatedItemDto = new ItemDto()
+            {
+                Id = 1,
+                Name = "Bread updated",
+                NextReplenishmentDate = new DateTime(2020, 10, 10),
+                ReplenishmentPeriod = 4
+            };
+
+
+            //Act
+            var result = ItemsDataService.UpdateItem("ab70793b-cec8-4eba-99f3-cbad0b1649d0", updatedItemDto);
+
+
+            //Assert
+            AssertHelper.AssertAll(
+                () => result.IsSuccess.Should().BeTrue(),
+                () => result.ErrorMessage.Should().BeNull()
+                );
         }
     }
 }
