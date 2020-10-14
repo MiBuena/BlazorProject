654e8869a5fcced5dc5b34f0cd355ebeeb066a65
MiBuena <athina.palada@gmail.com>
2020-10-14 19:55:32 +0300

    Add test for ItemDto null check

--- ListGenerator.Server/ListGenerator.Web.UnitTests/ItemsDataServiceTests/UpdateItemTests.cs
+++ ListGenerator.Server/ListGenerator.Web.UnitTests/ItemsDataServiceTests/UpdateItemTests.cs
@@ -248,9 +248,7 @@ namespace ListGenerator.Web.UnitTests.ItemsDataServiceTests
             var allItems = BuildItemsCollection();
             ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
 
-            ItemsRepositoryMock
-                .Setup(c => c.Update(null));
-
+            ItemsRepositoryMock.Setup(c => c.Update(It.IsAny<Item>()));
             ItemsRepositoryMock.Setup(c => c.SaveChanges());
 
 
@@ -258,12 +256,40 @@ namespace ListGenerator.Web.UnitTests.ItemsDataServiceTests
             var result = ItemsDataService.UpdateItem("ab70793b-cec8-4eba-99f3-cbad0b1649d0", null);
 
 
-            ItemsRepositoryMock.Verify(x => x.All(), Times.Never());
-
             //Assert
             AssertHelper.AssertAll(
                 () => result.IsSuccess.Should().BeFalse(),
                 () => result.ErrorMessage.Should().Be("An error occurred while updating item")
+                );
+        }
+
+        [Test]
+        public void Should_CheckForItemDtoNullBeforeAllOtherMethodCalls_When_ItemDtoIsNull()
+        {
+            //Arrange
+            var allItems = BuildItemsCollection();
+            ItemsRepositoryMock.Setup(x => x.All()).Returns(allItems);
+
+            ItemsRepositoryMock.Setup(c => c.Update(It.IsAny<Item>()));
+            ItemsRepositoryMock.Setup(c => c.SaveChanges());
+
+            var updatedItemDto = new ItemDto()
+            {
+                Id = 1,
+                Name = "Bread updated",
+                NextReplenishmentDate = new DateTime(2020, 10, 10),
+                ReplenishmentPeriod = 4
+            };
+
+            //Act
+            var result = ItemsDataService.UpdateItem("ab70793b-cec8-4eba-99f3-cbad0b1649d0", null);
+
+
+            //Assert
+            AssertHelper.AssertAll(
+                () => ItemsRepositoryMock.Verify(x => x.All(), Times.Never()),
+                () => ItemsRepositoryMock.Verify(x => x.Update(It.IsAny<Item>()), Times.Never()),
+                () => ItemsRepositoryMock.Verify(x => x.SaveChanges(), Times.Never())
                 );
         }
     }
